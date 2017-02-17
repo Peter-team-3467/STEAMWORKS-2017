48c9b926a009b74b4d8d451c91ad12bae52b81bb
Peter-team-3467 <palborghetti@team3467.org>
2017-02-17 17:26:31 -0500

    Fixing stuff

--- src/org/usfirst/frc3467/subsystems/HighIntake/HighIntake.java
+++ src/org/usfirst/frc3467/subsystems/HighIntake/HighIntake.java
@@ -6,23 +6,23 @@ import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class HighIntake1 extends Subsystem {
+public class HighIntake extends Subsystem {
     
 	public CANTalon upperIntake;
 	public DoubleSolenoid hisolenoid;
 	
-	public HighIntake1() {
+	public HighIntake() {
 		upperIntake = new CANTalon(8);
 		hisolenoid = new DoubleSolenoid(3, 4);
 		//port numbers, placeholder values currently
 	}
     
-    public void deploy(DoubleSolenoid hisolenoid) {
+    public void in(DoubleSolenoid hisolenoid) {
     	hisolenoid.set(DoubleSolenoid.Value.kForward);
     	
     }
     
-    public void retract(DoubleSolenoid hisolenoid) {
+    public void out(DoubleSolenoid hisolenoid) {
     	hisolenoid.set(DoubleSolenoid.Value.kReverse);
   	
     }
